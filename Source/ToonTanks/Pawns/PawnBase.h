cfc2c414072ad6885ac58e90ac18a0645ace8db9
Garrett Thompson <drummergmt@gmail.com>
2021-07-02 12:21:58 -0400

    Added Pawn Tank C++ Files
    Incorporated Movement

--- Source/ToonTanks/Pawns/PawnBase.h
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -26,15 +26,4 @@ class TOONTANKS_API APawnBase : public APawn
 public:
 	// Sets default values for this pawn's properties
 	APawnBase();
-
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;	
-
 };
